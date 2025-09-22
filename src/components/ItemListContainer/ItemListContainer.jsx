1a3ed942bdf2e863ffa1134be469789178ef71b6
leonardo alva <leeoalva@gmail.com>
2025-09-22 11:42:18 -0300

    props app-itemList

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,22 @@
-const ItemListContainer = () => {
-  return <div>ItemListContainer</div>;
-}
+import "./ItemListContainer.css";
+
+const ItemListContainer = ({ titulo, productos }) => {
+  return (
+    <>
+      <h1>{titulo}</h1>
+      <p>Todos los ESports en un solo lugar </p>
+
+      <div className="itemList_grid">
+        {productos.map((prod) => (
+          <article key={prod.id}>
+            <h3>{prod.nombre}</h3>
+            <span className="precio_card">${prod.precio}</span>
+            <p className="descripcion_card">{prod.descripcion}</p>
+          </article>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default ItemListContainer;
