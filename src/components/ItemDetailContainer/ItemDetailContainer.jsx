18c3fe0e8f8d92d3ccb166eb137928ebaabd45e5
leonardo alva <leeoalva@gmail.com>
2025-10-23 08:45:35 -0300

    PreEnt Add-ItemDetail

---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ItemDetailContainer = () => {
+  return (
+    <div>
+      <h2>Detalle del Producto</h2>
+      {/* Aquí se mostrará la información del producto seleccionado */}
+    </div>
+  );
+};
+
+export default ItemDetailContainer;
